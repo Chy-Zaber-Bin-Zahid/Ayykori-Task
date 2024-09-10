581b8e2ceddf91dd1076c7846b3cf19b7bd5c2eb
Chy-Zaber-Bin-Zahid <89964035+Chy-Zaber-Bin-Zahid@users.noreply.github.com>
2024-09-10 18:15:28 +0600

    task1 page created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
-    <div>
-      Hello
+    <div className="w-full h-screen flex justify-center items-center gap-4" >
+      <Link href="/task1" ><button className="bg-blue-500 p-2 rounded text-white transition-all duration-300 hover:bg-blue-600">Recursive-partitioning (Task 1)</button></Link>
+      <Link href="/task2" ><button className="bg-blue-500 p-2 rounded text-white transition-all duration-300 hover:bg-blue-600">Alphabet Tile Interaction (Task 2)</button></Link>
     </div>
   );
 }
---
+++ src/app/task1/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TaskOne() {
+  return (
+    <div>TaskOne</div>
+  )
+}
+
+export default TaskOne
